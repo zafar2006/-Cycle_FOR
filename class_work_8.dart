8c00343bc09c54759e3622241b4f90cb8075c413
zafar2006 <kasimovzafar683@gmail.com>
2023-11-24 16:16:37 +0600

    second comit

--- class_work_8.dart
+++ class_work_8.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main() {
+//                    КЛАССНАЯ РАБОТА:
   // for (int i = 0; i < 10; i++) {
   //   print(i);
   // }
@@ -10,16 +11,20 @@ void main() {
   //   print(data[i].toUpperCase());
   // }
 
-  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89, 21];
-  List result = [];
-  for (int i = 0; i < a.length; i++) {
-    if (b.contains(a[i])) {
-      result.add(a[i]);
-    }
-  }
-  print(result.toSet().toList());
+  //                       ЗАДАЧИ:
+//1)ЗАДАЧА
 
+  // List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  // List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89, 21];
+  // List result = [];
+  // for (int i = 0; i < a.length; i++) {
+  //   if (b.contains(a[i])) {
+  //     result.add(a[i]);
+  //   }
+  // }
+  // print(result.toSet().toList());
+
+//2)ЗАДАЧА
   // List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
   // List result = [];
   // for (int i = 0; i < a.length; i++) {
@@ -29,6 +34,7 @@ void main() {
   // }
   // print(result);
 
+//3)ЗАДАЧА
   // ;
   // print('Введите первую строку');
   // String a = stdin.readLineSync()!;
@@ -41,68 +47,4 @@ void main() {
   //   }
   // }
   // print(counter);
-
-  // int hourse = 345;
-  // if (hourse < 1000) {
-  //   print('Первая часть');
-  // } else if (hourse < 2000) {
-  //   print('Вторая часть');
-  // } else if (hourse < 3000) {
-  //   print('Третья часть');
-  // } else if (hourse < 4000) {
-  //   print('Четвертая часть');
-  // } else if (hourse > 4000) {
-  //   print('Ошибка');
-  // }
-
-  // stdout.write("Введите первое число: ");
-  // int firstNumber = int.parse(stdin.readLineSync()!);
-
-  // stdout.write("Введите второе число: ");
-  // int secondNumber = int.parse(stdin.readLineSync()!);
-
-  // if (firstNumber < secondNumber) {
-  //   print("Наименьшее число: $firstNumber");
-  // } else if (secondNumber < firstNumber) {
-  //   print("Наименьшее число: $secondNumber");
-  // } else {
-  //   print("Введенные числа равны.");
-  // }
-  // String f = ('Введите свой возрвст');
-  // print(f);
-  // int age = int.parse(stdin.readLineSync()!);
-  // if (age < 18) {
-  //   print('Извените вы слишком молоды для вождения автомобиля');
-  // } else if (age >= 18) {
-  //   print('вы допущены для вождения автомобиля');
-  // }
-  // String d = ('Введите целое число');
-  // print(d);
-  // int numde = int.parse(stdin.readLineSync()!);
-  // if (numde % 2 == 0) {
-  //   print('число четное');
-  // } else {
-  //   print('число нечетное');
-  // }
-  // String h = ('Ведите текущее время в часах:');
-  // print(h);
-  // double hourse = double.parse(stdin.readLineSync()!);
-  // if (hourse < 6) {
-  //   print('сейчас ранее утро');
-  // } else if (hourse < 12.00) {
-  //   print('сейчас утро');
-  // } else if (hourse < 18.00) {
-  //   print('Сейчас день');
-  // } else if (hourse < 20.00) {
-  //   print('сейчас вечер');
-  // } else if (hourse < 23.00) {
-  //   print('сейчас ночь');
-  // }
-  // print('Ведите число');
-  // int a = int.parse(stdin.readLineSync()!);
-  // if (a % 2 == 0) {
-  //   print('Четное число');
-  // } else {
-  //   print('Нечетное число');
-  // }
 }
